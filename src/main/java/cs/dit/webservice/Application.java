55229d0d3d0dc781c7212695cd14433ec1306356
jinsook <putzmunter7@gmail.com>
2021-09-09 22:23:05 +0900

    Initial commit

---
+++ src/main/java/cs/dit/webservice/Application.java
@@ -0,0 +1,11 @@
+package cs.dit.webservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String arg[]){
+        SpringApplication.run(Application.class, arg);
+    }
+
+}
